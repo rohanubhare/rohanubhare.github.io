e26558caea0e11e00d4e9e2320404c31a6cb3de3
rohanubhare <rohan.ubhare@gmail.com>
2025-04-18 18:37:55 +0530

    Add improvements

--- asymmetri-task/flutter_service_worker.js
+++ asymmetri-task/flutter_service_worker.js
@@ -23,14 +23,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
-"flutter_bootstrap.js": "f130467c701027f5646dfe4bf4d51a24",
+"flutter_bootstrap.js": "1fc137f11f29b373faa4ab23433bf1ec",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "a249dce2aea58183e527dada9ffab764",
 "/": "a249dce2aea58183e527dada9ffab764",
-"main.dart.js": "1d45ffa670c91eead722f56a0a8cc2e9",
+"main.dart.js": "04e935a469908138048540a51fee6e72",
 "manifest.json": "6ec6b26532e84c75063728fe05b30fe8",
 "version.json": "d280789c783cefaea18379602fbd9f32"};
 // The application shell files that are downloaded before a service worker can
